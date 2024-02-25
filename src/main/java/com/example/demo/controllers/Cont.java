d118c51b67717b6a2a8543e9d602cb565a5f4007
PrethamMuthappa <prethambachira1819@gmail.com>
2024-02-25 21:23:56 +0530

    added index

--- src/main/java/com/example/demo/controllers/Cont.java
+++ src/main/java/com/example/demo/controllers/Cont.java
@@ -1,12 +1,14 @@
 package com.example.demo.controllers;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/users/v1")
 public class Cont {
     @GetMapping("/")
-    public String hello(){
-        return "HOME PAGE";
+    public ModelAndView hello(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 
 }
